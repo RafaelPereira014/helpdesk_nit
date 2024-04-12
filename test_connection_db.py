59926ac1a42224bcb1d23c5d83d7f1ecf2072ca8
RafaelPereira014 <rafaelbotelhopereira@ua.pt>
2024-04-12 15:13:05 +0000

    new user creation working

--- test_connection_db.py
+++ test_connection_db.py
@@ -27,7 +27,7 @@ try:
         
         is_edu = check_email_contains_edu('1')
 
-        cursor.execute("SELECT * from tickets  ")
+        cursor.execute("SELECT * from users")
         name = cursor.fetchall()
         print(name)
         # Fetch all rows (tickets) from the result set
